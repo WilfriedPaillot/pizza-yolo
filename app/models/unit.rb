a42af2eba13a91e71876487c4e692eab7d1e06e6
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:29:14 +0200

    Update unit model w/ validations

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,3 +1,10 @@
 class Unit < ApplicationRecord
   has_many :ingredients
+
+  validates :name, presence: true,
+    length: { minimum: 3, maximum: 25 },
+    format: { with: /\A[a-zA-Z\s'-]+\z/ }
+  validates :tag, presence: true,
+    length: { minimum: 1, maximum: 3 },
+    format: { with: /\A[a-zA-Z]+\z/ }
 end
