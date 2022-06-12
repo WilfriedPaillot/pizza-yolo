88c91a2c469614f4e1721dfc7768171a978d4591
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:48:36 +0200

    Update User model validation for custom fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,27 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :restaurants
   has_one :cart
+
+  validates :first_name, :last_name, :address, :zipcode, :city, :email, :phone,
+    allow_blank: true, 
+  on: :create
+  validates :first_name, :last_name, 
+    presence: true,
+    length: { minimum: 2, maximum: 20, message: "doit être compris entre 2 et 20 caractères" }, 
+    format: { with: /\A[a-zA-Z\s\-\']+\z/ , message: "ne doit contenir que des lettres" },
+  on: :update
+  validates :address, 
+    presence: true,
+    length: { minimum: 5, maximum: 100 },
+  on: :update
+  validates :zipcode,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 01000, less_or_equal_to: 99138 },
+    length: { is: 5 },
+  on: :update
+  validates :city,
+    presence: true,
+    length: { minimum: 2, maximum: 20 },
+    format: { with: /\A[a-zA-Z\-\'\s]+\z/ },
+  on: :update
 end
