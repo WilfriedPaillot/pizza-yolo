baa06fa10eeb0592b1f048fc6b4f38d6f361f2f5
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:25:09 +0200

    Update comment model w/ validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
+
+  validates :quote, presence: true,
+    length: { in: 1..5 },
+    numericality: { only_integer: true, greater_than: 0, less_or_equal_to: 5 }
+  validates :message, presence: true,
+    length: { minimum: 3, maximum: 160 }
 end
