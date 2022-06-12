4193390c52e1ab7550ae2ddda0166fa1b7210584
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:22:13 +0200

    Update ingredient model w/ validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,8 @@ class Ingredient < ApplicationRecord
   belongs_to :unit
   has_many :product_ingredients
   has_many :products, through: :product_ingredients
+
+  validates :name, presence: true,
+    length: { minimum: 3, maximum: 50 },
+    format: { with: /\A[a-zA-Z\s'-]+\z/ }
 end
