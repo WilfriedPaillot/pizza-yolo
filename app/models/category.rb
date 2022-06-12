9d067d457df41b499e14a1402dea129a3146e55c
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:26:48 +0200

    Update category model w/ validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   has_many :products
+
+  validates :name, presence: true,
+    length: { minimum: 3, maximum: 50 },
+    format: { with: /\A[a-zA-Z\s'-]+\z/ }
+  validates :description, presence: true,
+    length: { minimum: 3, maximum: 150 }
 end
