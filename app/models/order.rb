7b9e9649ff90ed1bf47ef6a055700e1e73c9f802
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:21:04 +0200

    Update order model w/ validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_products
   has_many :products, through: :order_products
+
+  validates :references, presence: true,
+    length: { minimum: 5, maximum: 255 },
+    format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :total, presence: true,
+    length: { minimum: 1, maximum: 7 },
+    numericality: { greater_or_equal_to: 0, less_or_equal_to: 99999.99 },
+    format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
 end
