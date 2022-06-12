bb15031995d74e5b1b077e2159b6f6e98d4f5143
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 23:16:16 +0200

    Update product model w/ validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,18 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_products
   has_many :product_ingredients
   has_many :ingredients, through: :product_ingredients
+
+  validates :name, presence: true,
+    length: { minimum: 3, maximum: 50 },
+    format: { with: /\A[a-zA-Z\s'-]+\z/ },
+    uniqueness: { case_sensitive: false }
+  validates :description, presence: true,
+    length: { minimum: 3, maximum: 150 },
+    format: { with: /\A[a-zA-Z\s'-]+\z/ }
+  validates :price, presence: true,
+    length: { minimum: 1, maximum: 5 },
+    format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+  validates :image_url, presence: true,
+    length: { maximum: 255 },
+    format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix }
 end
