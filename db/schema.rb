518704ad4f632d604026a68b2027ac6d8c3b90f1
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 19:50:57 +0200

    Generate & migrate Unit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_12_174853) do
+ActiveRecord::Schema.define(version: 2022_06_12_175030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2022_06_12_174853) do
     t.string "city", null: false
     t.string "email", null: false
     t.string "phone", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "tag", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
