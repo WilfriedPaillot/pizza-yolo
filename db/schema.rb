b1a6a8071d2c8da5c63194ebaadfbd377a15dc39
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-12 21:11:31 +0200

    generate migration to add ref in Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_12_190634) do
+ActiveRecord::Schema.define(version: 2022_06_12_191009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 2022_06_12_190634) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["restaurant_id"], name: "index_products_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -86,4 +90,6 @@ ActiveRecord::Schema.define(version: 2022_06_12_190634) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "restaurants"
 end
