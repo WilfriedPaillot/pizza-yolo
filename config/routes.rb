511515284d703d481a2616a767de73536521b0e3
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-13 10:56:50 +0200

    Add carts & cart_products resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :products
+  resources :carts
+  resources :cart_products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
