d0ba1b2a9f669e0de23c2ca30d7d7579e67e4160
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-13 10:56:04 +0200

    Generate carts controller w/ new create show methods + create total_price model method

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,39 @@
+class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @cart = Cart.new(user_id: params[:id])
+  end
+
+  def create
+    if current_user.cart == nil
+      @cart = Cart.new(cart_params)
+      @cart.user_id = current_user.id
+      @cart.save
+    else
+      @cart = current_user.cart
+    end
+  end
+
+  def show
+    # si le current_user a un cart, on le récupère
+    if current_user.cart != nil
+      @cart = current_user.cart
+      @products = @cart.cart_products
+    else
+      # sinon on crée un cart vide
+      @cart = Cart.new(user_id: current_user.id)
+      @cart.save!
+    end
+  end
+
+  private
+  def set_cart
+    @cart = Cart.find_by(user_id: params[:id])
+  end
+
+  def cart_params
+    params.require(:cart).permit(:user_id)
+  end
+
+end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,14 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_products
   has_many :products, through: :cart_products
+
+  
+  def total_price
+    total = 0
+    self.cart_products.each do |cart_product|
+      total += cart_product.product.price * cart_product.quantity
+    end
+    total
+  end
+
 end
