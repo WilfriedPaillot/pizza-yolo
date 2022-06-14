32e6ed02638998a2f82ddfbd8ce4f3910a431ecb
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-15 01:41:41 +0200

    Update application controller w/ permitted params for update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  add_flash_types :danger, :info, :warning, :success, :messages
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # Devise overrides
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update, 
+      keys: [
+        :firstname, 
+        :lastname, 
+        :email, 
+        :password, 
+        :password_confirmation, 
+        :current_password,
+        :firstname,
+        :lastname,
+        :address,
+        :zipcode,
+        :city,
+        :phone
+      ]
+    )
+  end
 end
