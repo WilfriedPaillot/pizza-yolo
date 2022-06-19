ecda37754881bbff358d6fdef98619174baa4dc7
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-20 01:09:24 +0200

    Update Unit model to fix validates

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,8 +3,8 @@ class Unit < ApplicationRecord
 
   validates :name, presence: true,
     length: { minimum: 3, maximum: 25 },
-    format: { with: /\A[a-zA-Z\s'-]+\z/ }
+    format: { with: /\A[a-zA-Z\s'-éàèêçù]+\z/ }
   validates :tag, presence: true,
     length: { minimum: 1, maximum: 3 },
-    format: { with: /\A[a-zA-Z]+\z/ }
+    format: { with: /\A[a-zA-Z\s'-éàèêçù]+\z/ }
 end
