775a43e602493638e5b58ebec3bc379b1fa7897e
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-25 23:26:55 +0200

    Update products#index request w/ group and sort_by

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,18 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.order(:price)
+    if params[:category_id].present?
+      @products = Product.where(category_id: params[:category_id]).includes(:category).group_by { |p| p.category }.map { |k, v| [k, v.sort_by(&:name)] }
+    else
+      @products = Product.includes(:category).group_by { |p| p.category }.sort_by { |category, products| category.id }
+      @products.each do |category, products|
+        products.sort_by! { |p| p.name }
+      end
+    end
+    respond_to do |format|
+      format.html { render :index }
+      format.js {}
+    end
   end
 
   def show
