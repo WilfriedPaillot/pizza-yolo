4ed179036335b0521988175668ee701a4395231d
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-25 12:15:02 +0200

    Add Virgil font + updta eassets/initializer + create custom button styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,6 +14,8 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'ja
 Rails.application.config.assets.paths << Rails.root.join('lib', 'assets', 'stylesheets')
 Rails.application.config.assets.paths << Rails.root.join('lib', 'assets', 'javascripts')
 
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
