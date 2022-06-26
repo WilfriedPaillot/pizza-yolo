3e59a72592214e94fac27f5ba5499d66f4d5a51b
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-26 17:39:28 +0200

    Update w/ resources: order_products

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :products
   resources :carts
   resources :cart_products
+  resources :order_products
+
   scope "/checkout" do
     post "create", to: "checkout#create", as: "checkout_create"
     get "cancel", to: "checkout#cancel", as: "checkout_cancel"
