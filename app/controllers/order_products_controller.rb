0a41fb0306a16c3ab75bd7cf328506b6f49ed837
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-27 00:51:15 +0200

    Delete controller OrderProducts w/ all dependencies files

--- app/controllers/order_products_controller.rb
+++
@@ -1,9 +0,0 @@
-class OrderProductsController < ApplicationController
-  def show
-    @order_products = OrderProduct.where(order_id: params[:order_id])
-    respond_to do |format|
-      format.html { render :show }
-      format.js {}
-    end
-  end
-end
