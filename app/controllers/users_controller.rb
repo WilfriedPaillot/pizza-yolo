29593ee1ad9f026c4f7209854de1d9a4591666dd
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-26 17:02:36 +0200

    Update Users controller w/ show edit update & params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,23 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @orders = @user.orders
+    @orders = @user.orders.order(created_at: :desc).limit(10)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = current_user.update(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Votre profil a été mis à jour.' }
+        format.js { }
+      else
+        format.html { render :edit, notice: 'Votre profil n\'a pas pu être mis à jour.' }
+      end
+    end
   end
 
   private
@@ -14,5 +30,7 @@ class UsersController < ApplicationController
     end
   end
 
-
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :address, :city, :phone, :zipcode, restaurants: [])
+  end
 end
