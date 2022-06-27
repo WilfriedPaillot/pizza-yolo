4a22c8551c8065d721957c0361a016c1a5a79afe
WilfriedPaillot <paillotwilfried@gmail.com>
2022-06-27 02:38:11 +0200

    Update staticPages#homepage controller w/ 110% functionnal request @best_sellers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def homepage
-    @products = Product.featured(3)
+    # products = OrderProduct.group(:product_id).sum(:quantity)
+    #   p "*" * 100
+    #   p "products: #{products}"
+    #   p "*" * 100
+    # products.each do |product|
+    #   p "*" * 100
+    #   p "Product: #{Product.find(product[0]).name} - Quantity: #{product[1]}"
+    #   p "*" * 100
+    # end
+    # @best_sellers = products.sort_by { |k, v| v }.reverse.take(3).map { |p| Product.find(p[0]) }
+    @best_sellers = OrderProduct.group(:product_id).sum(:quantity).sort_by { |k, v| v }.reverse.take(3).map { |k, v| Product.find(k) }
   end
 end
