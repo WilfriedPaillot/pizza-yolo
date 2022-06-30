e4e7ae3e9e1ac3fa4df662bbd27b061d0248ce39
WilfriedPaillot <paillotwilfried@gmail.com>
2022-07-01 00:11:04 +0200

    Generate migration to add columns longitude & latitude to Rastaurants

---
+++ db/migrate/20220630220801_add_coordinates_to_restaurants.rb
@@ -0,0 +1,6 @@
+class AddCoordinatesToRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :restaurants, :longitude, :float
+    add_column :restaurants, :latitude, :float
+  end
+end
