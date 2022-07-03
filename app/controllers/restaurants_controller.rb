32ad682779603340c3738f5b032b5c46c17f2eb0
WilfriedPaillot <paillotwilfried@gmail.com>
2022-07-04 01:50:56 +0200

    Update restaurant#index controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
+    @restaurants = Restaurant.order(:zipcode)
+  end
+
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
