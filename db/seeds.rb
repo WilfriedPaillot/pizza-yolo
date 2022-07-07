cceba1af37527b013d4627c2624e84eb2333bc4d
WilfriedPaillot <paillotwilfried@gmail.com>
2022-07-07 14:35:12 +0200

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,72 @@
 # Erase the content of all tables, hence reseting the related "id" counters
 DatabaseCleaner.clean_with(:truncation)
 
-
-# Create 10 random zipcodes between 01000 and 99138
-@zipcodes = []
-10.times do
-  if @zipcodes.empty? || @zipcodes.uniq.length != 10
-    zipcode = Faker::Number.leading_zero_number(digits: 5)
-    if zipcode.to_i > 01000 && zipcode.to_i < 98899 || zipcode.to_i == 99138
-      @zipcodes << zipcode
-      # puts "zipcodes: #{@zipcodes}"
-    end
-  end
-end
+# Create addresses for users
+@address = [
+  {
+    'zipcode' => '45240',
+    'city' => "La ferté saint aubin"
+  },
+  {
+    'zipcode' => '41600',
+    'city' => 'Lamotte-Beuvron'
+  },
+  {
+    'zipcode' => '45130',
+    'city' => 'Saint Ay'
+  },
+  {
+    'zipcode' => '45100',
+    'city' => 'Orléans'
+  },
+  {
+    'zipcode' => '45150',
+    'city' => 'Jargeau'
+  }
+]
 
 # Create 10 random phone numbers between 0100000000 and 0599999999
 @phones = []
 10.times do
   if @phones.empty? || @phones.uniq.length != 10
     phone = Faker::Number.number(digits: 9).to_s
-      if phone.to_i > 100000000 && phone.to_i < 599999999
-        phone = phone.insert(0, '0')
-        @phones << phone
-        # puts "phones: #{@phones}"
-      end
+    if phone.to_i > 100000000 && phone.to_i < 599999999
+      phone = phone.insert(0, '0')
+      @phones << phone
+    end
   end
+  puts "phones: #{@phones.last}"
 end
 
+  @address.each do |address|
+    p "Create restaurant w/ address: #{address['city']}"
+    Restaurant.create!(
+      name: 'Pizza Yolo',
+      address: Faker::Address.street_address,
+      zipcode: address['zipcode'],
+      city: address['city'],
+      email: address['city'].parameterize + '@yopmail.com',
+      phone: @phones.sample
+    )
+    p "La pizzeria #{Restaurant.last.name} has been created with the following information:"
+    p "Zip code: #{Restaurant.last.zipcode}"
+    p "City: #{Restaurant.last.city}"
+    p "Email: #{Restaurant.last.email}"
+    p "-----------------------------------------------------"
+    p ""
+  end
+
+
+#! ADMIN ACCOUNT
 User.create!(
   email: 'paillotwilfried@gmail.com',
   password: 'pizzayolo@2022',
   role: :admin
 )
-
 p "#{User.last.email} has been created as an #{User.last.role}"
 
-10.times do
+#! USER ACCOUNTS
+5.times do
   @first_name = Faker::Name.first_name
   @last_name = Faker::Name.last_name
 
@@ -45,97 +76,136 @@ p "#{User.last.email} has been created as an #{User.last.role}"
   )
   p "#{User.last.email} has been created as an #{User.last.role}"
   
+  @address[rand(0..@address.length - 1)] do |address|
   User.last.update!(
-    firstname: @first_name,
-    lastname: @last_name,
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
     address: Faker::Address.street_address,
-    zipcode: @zipcodes.sample,
-    city: Faker::Address.city,
+    zipcode: address['zipcode'],
+    city: address['city'],
     phone: @phones.sample,
-  )
-  p "#{User.last.email} has been updated with the following information:"
-  p "First name: #{User.last.firstname}"
-  p "Last name: #{User.last.lastname}"
-  p "Address: #{User.last.address}"
-  p "Zip code: #{User.last.zipcode}"
-  p "City: #{User.last.city}"
-  p "Phone: #{User.last.phone}"
-  p "----------------------------------------------------"
+    restaurant_id: Restaurant.find(rand(1..Restaurant.count)).id
+    )
+  end
 end
 
 p "#{User.where(role: 0).count} users have been created"
 p "-----------------------------------------------------"
 p ""
-Restaurant.create!(
-  name: 'Pizza Yolo',
-  address: Faker::Address.street_address,
-  zipcode: @zipcodes.sample,
-  city: Faker::Address.city,
-  phone: @phones.sample,
-  email: 'pizza-yolo@yopmail.com',
-  user_id: User.find_by(role: 1).id
-)
+User.where(role: 0).each do |user|
+  p "#{user.email} has been generate with the following information:"
+  p "First name: #{user.firstname}"
+  p "Last name: #{user.lastname}"
+  p "Address: #{user.address}"
+  p "Zip code: #{user.zipcode}"
+  p "City: #{user.city}"
+  p "Phone: #{user.phone}"
+  p "Favorite restaurant: #{user.restaurant_id}"
+  p ""
+end
 
-p "La pizzeria #{Restaurant.last.name} has been created with the following information:"
-p "Zip code: #{Restaurant.last.zipcode}"
-p "City: #{Restaurant.last.city}"
-p "Email: #{Restaurant.last.email}"
-p "-----------------------------------------------------"
-p ""
+@pizzas = [
+  {	'name' => 'Margarita',
+    'description' => 'La pizza Margherita est une spécialité culinaire traditionnelle de la ville de Naples, en Italie',
+    'price' => 8.50,
+  },
+  {	'name' => 'Marinara',
+    'description' => 'La pizza marinara est une spécialité italienne, un des deux types traditionnels de pizza napolitaine.',
+    'price' => 11.20,
+  },
+  {	'name' => 'Quattro formaggi',
+    'description' => 'La pizza quattro formaggi est une spécialité italienne, un des deux types traditionnels de pizza napolitaine.',
+    'price' => 12.50,
+  }
+]
 
-Category.create!(name: 'pizza', description: 'La carte des pizzas')
+Category.create!(name: 'pizza', description: 'Nos authentiques pizzas')
   p "Category #{Category.last.name} has been created"
   p "-----------------------------------------------------"
-  Category.create!(name: 'soda', description: 'Large choix de sodas')
+
+  @pizzas.each do |pizza|
+    Product.create!(
+      name: pizza['name'],
+      description: pizza['description'],
+      price: pizza['price'],
+      category: Category.last,
+      image_url: "https://via.placeholder.com/150"
+    )
+  end
+  
+
+Category.create!(name: 'boisson', description: 'Large choix de sodas')
   p "Category #{Category.last.name} has been created"
   p "-----------------------------------------------------"
-  Category.create!(name: 'vin', description: 'Carte des vins')
-  p "Category #{Category.last.name} has been created"
+  
+  @drinks = [
+    {	
+      'name' => 'Coca-Cola',
+      'description' => "Coca-Cola est une boisson non-alcoolisée produite par la société Coca-Cola."
+    },
+    {
+      'name' => 'Sprite',
+      'description' => "Sprite est une boisson non-alcoolisée produite par la société Coca-Cola."
+    },
+    {
+      'name' => 'Fanta',
+      'description' => "Fanta est une boisson non-alcoolisée produite par la société Coca-Cola."
+    },
+    {
+      'name' => 'Pepsi',
+      'description' => "Pepsi est une boisson non-alcoolisée produite par la société PepsiCo. ! Ahhhh sous le soleil de PepsiCo..."
+    }
+  ]
+
+  @drinks.each do |drink|
+    Product.create!(
+      name: drink['name'],
+      description: drink['description'],
+      price: rand(1..3).to_f,
+      category: Category.last,
+      image_url: "https://via.placeholder.com/150"
+    )
+  end
+
+  p "Product #{Product.last.name} has been created"
   p "-----------------------------------------------------"
   p ""
+  
+  
+  Category.create!(name: 'dessert', description: 'Spécialités de desserts')
+    p "Category #{Category.last.name} has been created"
+    p "-----------------------------------------------------"
+    p ""
 
-Product.create!(
-  name: "Pizza Margherita",
-  description: "La pizza Margherita est une spécialité culinaire traditionnelle de la ville de Naples, en Italie",
-  price: Faker::Number.decimal(l_digits: 2),
-  image_url: "https://medias.delarte.fr/media/sys_master/images/h7a/h75/8863144181790.png",
-  restaurant_id: Restaurant.last.id,
-  category_id: Category.find_by(name: 'pizza').id
-)
-p "Product #{Product.last.name} has been created"
+    @desserts = [
+      {	
+        'name' => 'Tiramisu',
+        'description' => "Tiramisu est un dessert au lait qui est préparé à partir de la crème brulée et des fruits frais."
+      },
+      {
+        'name' => 'Tarte aux pommes',
+        'description' => "Tarte aux pommes Maison"
+      },
+      {
+        'name' => 'Tarte Tatin',
+        'description' => "Tarte Tatin, spécialité de la ville de Lamotte-Beuvron dans le Loir-et-Cher."
+      }
+    ]
+
+    @desserts.each do |dessert|
+      Product.create!(
+        name: dessert['name'],
+        description: dessert['description'],
+        price: rand(3..6).to_f,
+        category: Category.last,
+        image_url: "https://via.placeholder.com/150"
+      )
+    end
+      
+      p "Product #{Product.last.name} has been created"
+      p "-----------------------------------------------------"
+      p ""
+
 p "-----------------------------------------------------"
-
-Product.create!(
-  name: "Pizza Marinara",
-  description: "La pizza marinara est une spécialité italienne, un des deux types traditionnels de pizza napolitaine.",
-  price: Faker::Number.decimal(l_digits: 2),
-  image_url: "https://speisekarte.menu/storage/media/contributions/2213802/conversions/contribution_gallery.jpg",
-  restaurant_id: Restaurant.last.id,
-  category_id: Category.find_by(name: 'pizza').id
-)
-p "Product #{Product.last.name} has been created"
+p "And voila!"
 p "-----------------------------------------------------"
-
-Product.create!(
-  name: "Pizza Quattro Formaggi",
-  description: "La pizza quattro formaggi est une spécialité italienne, un des deux types traditionnels de pizza napolitaine.",
-  price: Faker::Number.decimal(l_digits: 2),
-  image_url: "https://assets.afcdn.com/recipe/20200506/110673_w96h73c1cx3120cy2080cxb6240cyb4160.jpg",
-  restaurant_id: Restaurant.last.id,
-  category_id: Category.find_by(name: 'pizza').id
-)
-p "Product #{Product.last.name} has been created"
-p "-----------------------------------------------------"
-
-Product.create!(
-  name: "Pizza carbonara",
-  description: "La pizza carbonara est une spécialité italienne, un des deux types traditionnels de pizza napolitaine.",
-  price: Faker::Number.decimal(l_digits: 2),
-  image_url: "https://assets.afcdn.com/recipe/20210930/122322_w96h73c1cx848cy675cxb2074cyb1445.jpg",
-  restaurant_id: Restaurant.last.id,
-  category_id: Category.find_by(name: 'pizza').id
-)
-p "Product #{Product.last.name} has been created"
-p "-----------------------------------------------------"
-p ""
-
